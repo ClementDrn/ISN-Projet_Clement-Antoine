ebb6cce7e9fa15bba9b708db1b7f3cc57ce14d2d
antoineisn <61066725+antoineisn@users.noreply.github.com>
2020-04-02 13:45:22 +0200

    Update main.py

--- Jeu w14a/main.py
+++ Jeu w14a/main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import hero
+import hero                     # On importe tous les sous fichiers dans cette fenêtre, c'est à dire les fichiers: hero,stage,debug,graphics,window
 import stage
 import debug
 import graphics
@@ -31,15 +31,15 @@ hero.init((500, 400), (200, 400))
 graphics.init()
 
 
-# Détection des touches
-get_can().bind_all('<Right>', hero.hero1.p_droite)
-get_can().bind_all('<KeyRelease-Right>', hero.hero1.r_droite)
-get_can().bind_all('<Left>', hero.hero1.p_gauche)
-get_can().bind_all('<KeyRelease-Left>', hero.hero1.r_gauche)
-get_can().bind_all('<Up>', hero.hero1.p_saut)
-get_can().bind_all('<KeyRelease-Up>', hero.hero1.r_saut)
+# Dans cette partie du programme, on assimile les ordres donnés par le programme à une touche du clavier
+get_can().bind_all('<Right>', hero.hero1.p_droite)      # la flêche directionnel de droite sur le clavier est assimilé au mouvement droite du hero1
+get_can().bind_all('<KeyRelease-Right>', hero.hero1.r_droite)       # quand on arrête d'appuyer suer la touche le héro ne bouge plus 
+get_can().bind_all('<Left>', hero.hero1.p_gauche)       #la flêche directionnel de gauche sur le clavier est assimilé au mouvement gauche du hero1
+get_can().bind_all('<KeyRelease-Left>', hero.hero1.r_gauche)        # quand on arrête d'appuyer suer la touche le héro ne bouge plus 
+get_can().bind_all('<Up>', hero.hero1.p_saut)           #la flêche directionnel du haut sur le clavier est assimilé au mouvement saut du hero1 
+get_can().bind_all('<KeyRelease-Up>', hero.hero1.r_saut)            # quand on arrête d'appuyer suer la touche le héro ne bouge plus 
 
-get_can().bind_all('<d>', hero.hero2.p_droite)
+get_can().bind_all('<d>', hero.hero2.p_droite)          # De même ave le héro2 mais avec les touches z;q;s;d
 get_can().bind_all('<KeyRelease-d>', hero.hero2.r_droite)
 get_can().bind_all('<q>', hero.hero2.p_gauche)
 get_can().bind_all('<KeyRelease-q>', hero.hero2.r_gauche)
